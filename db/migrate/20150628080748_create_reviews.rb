4d1f4202447774168e5288b181b5d0b9a482656a
MR.Y <programming@yatsukouji.com>
2015-06-29 21:16:48 +0900

    マイグレーションファイルの編集。１つのmemoにユーザは１つのみreviewができる仕様を実装

--- db/migrate/20150628080748_create_reviews.rb
+++ db/migrate/20150628080748_create_reviews.rb
@@ -1,11 +1,16 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.text :body
+      t.text :body, null: false
       t.references :memo, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
+    add_index :reviews, [:memo_id, :user_id], unique: true
   end
 end
+
+#add_indexの設定がポイント
+#一人のユーザーは一つのレビューしかできない
+#reviewテーブルではmemo_idとuser_idの組み合わせは一対のみ
