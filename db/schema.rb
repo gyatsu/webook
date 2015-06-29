e437c085f053001b6a587a81cfb0719edab339cb
MR.Y <programming@yatsukouji.com>
2015-06-29 21:21:18 +0900

    rake db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628063937) do
+ActiveRecord::Schema.define(version: 20150628080748) do
 
   create_table "memos", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20150628063937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.integer  "memo_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["memo_id", "user_id"], name: "index_reviews_on_memo_id_and_user_id", unique: true
+  add_index "reviews", ["memo_id"], name: "index_reviews_on_memo_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
