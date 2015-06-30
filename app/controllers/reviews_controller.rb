846de29d2756ed3099fdc8f8bab6d4ee5d4c108a
MR.Y <programming@yatsukouji.com>
2015-06-30 12:20:03 +0900

    def createとprivateをreviewsコントローラに実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,24 @@ class ReviewsController < ApplicationController
     @memo = Memo.find(params[:memo_id])
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(
+    memo_id: params[:memo_id],
+    user_id: current_user.id,
+    body: review_params["body"]
+    )
+    if @review.save
+      redirect_to memo_url(@review.memo)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
+  end
 end
+
